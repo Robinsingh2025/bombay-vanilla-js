55d279b75a60f52d30d363fa7dafd3eb3031533e
Robin7374 <7374robin@gmail.com>
2019-10-01 18:48:23 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+fetchData() {
+    // fetch the JSON data from our "server" (mocked)
+    
+    // parse the json
+    // send the items array to renderRecipeLinks()
+}
+fetchData();
+renderRecipeLinks(items) {}
