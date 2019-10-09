4a34b9fe0ecb9b226a6602e49f60892344908015
Robin7374 <7374robin@gmail.com>
2019-10-09 08:20:29 -0600

    10/08  needs front end work done

--- recipe.js
+++ recipe.js
@@ -1,6 +1,6 @@
-const container = document.getElementById('container');
 const step_Container = document.getElementById('steps-Container');
 const alert = document.getElementById('error-Alert');
+const heading = document.getElementById('heading');
 
 function createNode(element) {
   return document.createElement(element); // Create the type of element you pass in the parameters
@@ -11,65 +11,15 @@ function append(parent, el) {
 }
 
 function renderRecipe(item) {
-  //TODO Step number
-
-  container.innerHTML = `<div class="wrapper card">
-  <header>
-    <h1 id="${item.id}" >${item.name}</h1>
-  </header>
-    ${(step_Container.innerHTML = item.steps.map(step => {
-      const recipes = `<div class="step">
-      <h2>Step Number ${step.number} </h2>
-      <h4>${step.name}</h4>
-    </div>
-    <div class="ingredient">
-      <h4>Ingredient</h4>
-    </div>
-    <div class="ingredient-list">
-      <div>
-        <ul>
-          ${step.ingredients.map(ingredient => {
-            return `<li><span>${ingredient.measurement} </span>${ingredient.ingredient}</li>`;
-          })}
-        </ul>
-      </div>
-    </div>
-    <div class="instructions">
-      <div class=""><h4>Instructions</h4></div>
-      <p>
-        ${step.instruction}
-      </p>
-    </div>
-    <div>
-      <div class="gallery">
-        <h4>Image</h4>
-        <img src="" alt="" />
-      </div>
-      <div class="video">
-        <h4>Video</h4>
-        <video width="320" height="240" controls>
-          <source src="/video/" type="video/mp4" />
-        </video>
-      </div>
-      <div class="tool">
-        <h4>Tools</h4>
-        <ul>
-        <li>${step.tools}</li>
-        </ul>
-        <img src="" alt="" />
-      </div>
-    </div>
-   `;
-      return recipes;
-    }))}
-`;
   // destructure the item object
-  // FIXME
+  const { id, name, steps } = item;
+
   // Use each property of the item to fill in the recipe template
   // Use the name property to fill in the text of the <h1>
   // TODO: get the h1 element by its id and set the inner text to name
   // Hint: you decide what the id is going to be. Set that in recipe.html
-  // FIXME
+  heading.innerText = name;
+
   // Render the steps into the steps container <div>
   // TODO: (in HTML) create a <div> in recipe.html to put the steps in
   // Hint: Create a div in recipe.html with a specific id for you to reference
@@ -82,10 +32,71 @@ function renderRecipe(item) {
   // For arrays like ingredients and tools, use map again inside the template
   // Join the array returned from the map into one string using .join("")
   // For bonus points, chain the array methods of steps.map().join('');
-  // FIXME
+  const stepsHTML = steps
+    .map(
+      step => `
+    <div class="step">
+      <h2>Step Number ${step.number}</h2>
+      <h3>${step.name}</h3>
+      <h4>Ingredients</h4>
+      <ul>
+        ${step.ingredients
+          .map(ingredient => {
+            if (ingredient.id) {
+              return `
+                <li>
+                  <a href="/recipe.html?recipe=${ingredient.id}">${ingredient.measurement} of ${ingredient.name}</a>
+                </li>
+              `;
+            } else {
+              return `
+                <li>${ingredient.measurement} of ${ingredient.name}</li>
+              `;
+            }
+          })
+          .join('')}
+      </ul>
+      <h4>Instructions</h4>
+      <p>
+        ${step.instruction}
+      </p>
+      <div>
+        <div class="gallery">
+          <h4>Image</h4>
+          ${step.images
+            .map(
+              image => `
+            <img src="${image}" alt="" />
+          `
+            )
+            .join('')}
+        </div>
+        <div class="video">
+          <h4>Video</h4>
+          ${step.videos
+            .map(
+              video => `
+            <video width="320" height="240" controls>
+              <source src="${video}" type="video/mp4" />
+            </video>
+          `
+            )
+            .join('')}
+        </div>
+        <div class="tool">
+          <h4>Tools</h4>
+          <ul>
+            ${step.tools.map(tool => `<li>${tool}</li>`).join('')}
+          </ul>
+        </div>
+      </div>
+    </div>
+  `
+    )
+    .join('');
   // Get the containing div you made by the id you gave it
   // Set the inner HTML of that container div to your steps HTML template string
-  // FIXME
+  step_Container.innerHTML = stepsHTML;
 }
 
 // Fetch just one recipe item by its id
@@ -93,18 +104,21 @@ function fetchItem(id) {
   // TODO: Set the main header to say that it's "Loading..."
   // Get the h1 element by its id and set the inner text to something like "Loading..."
   // Hint: you decide what the id is going to be. Set that in recipe.html
-  // FIXME
+  heading.innerText = 'Loading...';
   // Get the specific item's json data by using the id in the fetch url
-  // TODO: use the id variable instead of just this hardcoded butter_chicken id
-  const url = 'data/butter_chicken.json'; // FIXME
+  const url = `data/${id}.json`;
+  // TODO: Catch the 404 if the json isn't found
   fetch(url)
     .then(response => response.json())
     .then(json => {
       // Once we have the data, grab just the item object by destructuring json
-      // FIXME
-      // Pass
-      // the item object to the render function
+      const { item } = json;
+      // Pass the item object to the render function
       renderRecipe(item);
+    })
+    .catch(error => {
+      console.log(error);
+      heading.innerText = error;
     });
 }
 
@@ -130,16 +144,15 @@ const recipeId = params.get('recipe');
 
 console.log(recipeId);
 if (recipeId) {
-  fetchRecipeById(recipeId);
+  // fetchRecipeById(recipeId);
   fetchItem(recipeId);
 } else {
   const error = `ERROR: No recipe id. id = ${recipeId}`;
   // TODO: Leave a message for the user on the page
-  const body = document.getElementById('body');
   const div = createNode('div');
   div.innerHTML = error;
-  append(body, div);
+  append(document.body, div);
 
   console.log(alert);
-  // FIXME: document.getElementById("heading").innerText = error;
+  heading.innerText = error;
 }
